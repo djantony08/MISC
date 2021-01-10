193060c1fe30414c3c7a49c4fa40f1a44202163c
djantony08 <35871429+djantony08@users.noreply.github.com>
2021-01-10 12:26:51 +0000

    Add files via upload

---
+++ SpawnManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+
+    public static int enemyCount;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+		{
+            Instantiate(enemyPrefab);
+		}
+    }
+}
