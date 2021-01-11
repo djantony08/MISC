85dabb404dff3a2a8cb1f2f49d66aa1c095dd466
djantony08 <35871429+djantony08@users.noreply.github.com>
2021-01-11 08:59:16 +0000

    Add files via upload

---
+++ UtilityHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class UtilityHelper
+{
+    //In a static class important to 
+    //know you cannot inherit from anything
+
+    public static int myAge;
+
+    public static void CreateObject()
+	{
+        //Create a new primitive cube
+        GameObject.CreatePrimitive(PrimitiveType.Cube);
+	}
+
+    public static void SetPositionToZero(GameObject obj)
+	{
+        //Change position of obj
+        obj.transform.position = Vector3.zero;
+	}
+    
+}
